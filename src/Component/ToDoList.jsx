52fe6fe05e4815dfa2b22068b4c40333bc5934f5
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-04 12:50:57 +0300

    added edit and delete

--- src/Component/ToDoList.jsx
+++ src/Component/ToDoList.jsx
@@ -1,13 +1,29 @@
 
 import ToDoItem from "./ToDoItem";
 import "./style.css"
-function ToDoList(props){
-    return (<>
-    <div className="list">
-    {props.itemsData.map((data)=> (<ToDoItem key={data.id} itemDetails={data} />) )}
-    </div>
-    
-    </>)
+function ToDoList({ todos, onToggleComplete, onDelete, onEdit }) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <ToDoItem
+          key={todo.id}
+          todo={todo}
+          onToggleComplete={onToggleComplete}
+          onDelete={onDelete}
+          onEdit={onEdit}
+        />
+      ))}
+    </ul>
+  );
 }
+// function ToDoList({arr,onComplete,onDelete,onEdit}){
+//     return (<>
+//     <div className="list">
+//     {arr.map((data)=> (<ToDoItem key={data.id} arr={data} onComplete={onComplete} onDelete={onDelete} onEdit={onEdit}/>) )}
+    
+//     </div>
+    
+//     </>)
+// }
 
 export default ToDoList;
