c59333bc0b6cd5f07e88e064280002f00ec39084
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-05 14:01:37 +0300

    added styles

--- src/Component/ToDoItem.jsx
+++ src/Component/ToDoItem.jsx
@@ -3,7 +3,8 @@ import { useState } from 'react'
 
 
 function ToDoItem({ todo, onToggleComplete, onDelete, onEdit }) {
-     const [isEditing, setIsEditing] = useState(false);
+   
+  const [isEditing, setIsEditing] = useState(false);
   const [editValue, setEditValue] = useState(todo.text);
 
   const handleSave = () => {
@@ -12,90 +13,27 @@ function ToDoItem({ todo, onToggleComplete, onDelete, onEdit }) {
   };
 
   return (
-    <li style={{ marginTop: "10px" }}>
-      {/* EDIT MODE */}
+    
+    <div className="items">
+      
       {isEditing ? (
         <>
-          <input
-            type="text"
-            value={editValue}
-            onChange={(e) => setEditValue(e.target.value)}
-          />
+          <input type="text" value={editValue} onChange={(e) => setEditValue(e.target.value)}/>
           <button onClick={handleSave}>Save</button>
         </>
       ) : (
         <>
-          {/* TEXT (Click to toggle complete) */}
-          <span
-            onClick={() => onToggleComplete(todo.id)}
-            style={{
-              textDecoration: todo.completed ? "line-through" : "none",
-              cursor: "pointer",
-              marginRight: "10px",
-            }}
-          >
-            {todo.text}
-          </span>
-
-          {/* EDIT BUTTON */}
-          <button onClick={() => setIsEditing(true)}>Edit</button>
+          <span style={{fontSize:30,color:"burlywood"}}>{todo.text}</span>
+           
+          <button className="btn2" onClick={() => onToggleComplete(todo.id)}>
+            {todo.completed && "✔"}
+          </button>
+          <button onClick={() => setIsEditing(true)}>✍</button>
         </>
       )}
-
-      {/* DELETE BUTTON */}
-      <button onClick={() => onDelete(todo.id)}>Delete</button>
-    </li>
+      <button onClick={() => onDelete(todo.id)}>🗑️</button>
+    </div>
   );
 }
-
-
-//      const [isEditing, setIsEditing] = useState(false);
-//   const [editValue, setEditValue] = useState(arr.name);
-  
-
-//   const handleSave = () => {
-//     onEdit(arr.id, editValue);
-//     setIsEditing(false);
-//   };
-
-
-//   return (
-//     <li style={{ marginTop: "10px" }}>
-//       {/* EDIT MODE */}
-//       <span>{arr.name}</span>
-//       {isEditing ? (
-//         <>
-//           <input
-//             type="text"
-//             value={editValue}
-//             onChange={(e) => setEditValue(e.target.value)}
-//           />
-//           <button onClick={handleSave}>Save</button>
-//         </>
-//       ) : (
-//         <>
-//           {/* TEXT (Click to toggle complete) */}
-//           <span
-//             onClick={() => onComplete(arr.id)}
-//             style={{
-//               textDecoration: arr.completed ? "line-through" : "none",
-//               cursor: "pointer",
-//               marginRight: "10px",
-//             }}
-//           >
-//             {arr.name}
-//           </span>
-
-//           {/* EDIT BUTTON */}
-//           <button onClick={() => setIsEditing(true)}>Edit</button>
-//         </>
-//       )}
-
-//       {/* DELETE BUTTON */}
-//       <button onClick={() => onDelete(arr.id)}>Delete</button>
-//     </li>
-//   );
-    
-
 
 export default ToDoItem;
