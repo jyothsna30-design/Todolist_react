850ce31b47279492fd9eda870271d5e9ea4ebc56
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-08 09:02:00 +0300

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import ToDoList from './Component/ToDoList'
 
+import Header from './Component/Header.jsx'
 import "./Component/style.css"
 
 function App() {
   const [arr, setArr] = useState([]);
   const [inputValue, setInputValue] = useState("");
 
- 
+ //Adding items to the list
   const handleAddItem = () => {
     if (inputValue.trim() === "") return;
 
@@ -24,18 +25,19 @@ function App() {
     setInputValue("");
   };
 
-  
+
+  //marking as complete
   const handleToggleComplete = (id) => {
     const updated = arr.map((todo) => todo.id === id ? { ...todo, completed: !todo.completed } : todo);
     setArr(updated);
   };
 
- 
+ //deletion
   const handleDelete = (id) => {
     setArr(arr.filter((todo) => todo.id !== id));
   };
 
-  
+  //Editing
   const handleEdit = (id, newText) => {
     const updated = arr.map((todo) => todo.id === id ? { ...todo, text: newText } : todo);
     setArr(updated);
@@ -43,7 +45,7 @@ function App() {
 
   return (
     <div>
-      <h1>To-Do List</h1>
+      <Header></Header>
       <input type="text" placeholder="Enter a task" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
       <button onClick={handleAddItem}>Add</button>
       <ToDoList todos={arr} onToggleComplete={handleToggleComplete} onDelete={handleDelete} onEdit={handleEdit}/>
